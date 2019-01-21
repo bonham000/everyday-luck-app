6495c1f0585305e273126362641c846551cb9d15
Sean Smith <sean.smith@tenx.tech>
2019-01-22 01:18:58 +0800

    Update content

--- src/content/mandarin/01.ts
+++ src/content/mandarin/01.ts
@@ -16,101 +16,106 @@ const Content: ReadonlyArray<Word> = [
     phonetic: "Yú",
     english: "Fish",
   },
-  // {
-  //   characters: "道",
-  //   phonetic: "Dào",
-  //   english: "Road",
-  // },
-  // {
-  //   characters: "歡",
-  //   phonetic: "Huān",
-  //   english: "Happy",
-  // },
-  // {
-  //   characters: "喜",
-  //   phonetic: "Xǐ",
-  //   english: "Like",
-  // },
-  // {
-  //   characters: "人",
-  //   phonetic: "Rén",
-  //   english: "People",
-  // },
-  // {
-  //   characters: "家",
-  //   phonetic: "Jiā",
-  //   english: "Family",
-  // },
-  // {
-  //   characters: "大",
-  //   phonetic: "Dà",
-  //   english: "Big",
-  // },
-  // {
-  //   characters: "餐",
-  //   phonetic: "Cān",
-  //   english: "Meal",
-  // },
-  // {
-  //   characters: "早",
-  //   phonetic: "Zǎo",
-  //   english: "Early",
-  // },
-  // {
-  //   characters: "上",
-  //   phonetic: "Shàng",
-  //   english: "On",
-  // },
-  // {
-  //   characters: "久",
-  //   phonetic: "Jiǔ",
-  //   english: "Long",
-  // },
-  // {
-  //   characters: "見",
-  //   phonetic: "Jiàn",
-  //   english: "See",
-  // },
-  // {
-  //   characters: "再見",
-  //   phonetic: "Zàijiàn",
-  //   english: "Goodbye",
-  // },
-  // {
-  //   characters: "再",
-  //   phonetic: "Zài",
-  //   english: "Again",
-  // },
-  // {
-  //   characters: "可",
-  //   phonetic: "Kě",
-  //   english: "Can",
-  // },
-  // {
-  //   characters: "愛",
-  //   phonetic: "Ài",
-  //   english: "Love",
-  // },
-  // {
-  //   characters: "為",
-  //   phonetic: "Wèi",
-  //   english: "For",
-  // },
-  // {
-  //   characters: "認",
-  //   phonetic: "Rèn",
-  //   english: "Recognize, know, admit",
-  // },
-  // {
-  //   characters: "超",
-  //   phonetic: "Chāo",
-  //   english: "Super",
-  // },
-  // {
-  //   characters: "對",
-  //   phonetic: "Duì",
-  //   english: "Correct",
-  // },
+  {
+    characters: "道",
+    phonetic: "Dào",
+    english: "Road",
+  },
+  {
+    characters: "歡",
+    phonetic: "Huān",
+    english: "Happy",
+  },
+  {
+    characters: "喜",
+    phonetic: "Xǐ",
+    english: "Like",
+  },
+  {
+    characters: "人",
+    phonetic: "Rén",
+    english: "People",
+  },
+  {
+    characters: "家",
+    phonetic: "Jiā",
+    english: "Family",
+  },
+  {
+    characters: "大",
+    phonetic: "Dà",
+    english: "Big",
+  },
+  {
+    characters: "餐",
+    phonetic: "Cān",
+    english: "Meal",
+  },
+  {
+    characters: "早",
+    phonetic: "Zǎo",
+    english: "Early",
+  },
+  {
+    characters: "上",
+    phonetic: "Shàng",
+    english: "On",
+  },
+  {
+    characters: "久",
+    phonetic: "Jiǔ",
+    english: "Long",
+  },
+  {
+    characters: "見",
+    phonetic: "Jiàn",
+    english: "See",
+  },
+  {
+    characters: "再見",
+    phonetic: "Zàijiàn",
+    english: "Goodbye",
+  },
+  {
+    characters: "再",
+    phonetic: "Zài",
+    english: "Again",
+  },
+  {
+    characters: "可",
+    phonetic: "Kě",
+    english: "Can",
+  },
+  {
+    characters: "愛",
+    phonetic: "Ài",
+    english: "Love",
+  },
+  {
+    characters: "為",
+    phonetic: "Wèi",
+    english: "For",
+  },
+  {
+    characters: "認",
+    phonetic: "Rèn",
+    english: "Recognize, know, admit",
+  },
+  {
+    characters: "超",
+    phonetic: "Chāo",
+    english: "Super",
+  },
+  {
+    characters: "對",
+    phonetic: "Duì",
+    english: "Correct",
+  },
+  {
+    characters: "天",
+    phonetic: "Tiān",
+    english: "Day",
+  },
 ];
 
 export default Content;
--- src/content/mandarin/02.ts
+++ src/content/mandarin/02.ts
@@ -16,71 +16,76 @@ const Content: ReadonlyArray<Word> = [
     phonetic: "Xièxiè",
     english: "Thank you",
   },
-  // {
-  //   characters: "不好",
-  //   phonetic: "Bù hǎo",
-  //   english: "Not good",
-  // },
-  // {
-  //   characters: "老師",
-  //   phonetic: "Lǎoshī",
-  //   english: "Teacher",
-  // },
-  // {
-  //   characters: "經理",
-  //   phonetic: "Jīnglǐ",
-  //   english: "Manager",
-  // },
-  // {
-  //   characters: "同事",
-  //   phonetic: "Tóngshì",
-  //   english: "Colleague",
-  // },
-  // {
-  //   characters: "早餐",
-  //   phonetic: "Zǎocān",
-  //   english: "Breakfast",
-  // },
-  // {
-  //   characters: "午餐",
-  //   phonetic: "Wǔcān",
-  //   english: "Lunch",
-  // },
-  // {
-  //   characters: "晚餐",
-  //   phonetic: "Wǎncān",
-  //   english: "Dinner",
-  // },
-  // {
-  //   characters: "什麼",
-  //   phonetic: "Shénme",
-  //   english: "What",
-  // },
-  // {
-  //   characters: "喜歡",
-  //   phonetic: "Xǐhuān",
-  //   english: "Like",
-  // },
-  // {
-  //   characters: "國家",
-  //   phonetic: "Guójiā",
-  //   english: "Country",
-  // },
-  // {
-  //   characters: "快樂",
-  //   phonetic: "Kuàilè",
-  //   english: "Happy",
-  // },
-  // {
-  //   characters: "可愛",
-  //   phonetic: "Kě'ài",
-  //   english: "Cute",
-  // },
-  // {
-  //   characters: "大哥",
-  //   phonetic: "Dàgē",
-  //   english: "Big brother",
-  // },
+  {
+    characters: "不好",
+    phonetic: "Bù hǎo",
+    english: "Not good",
+  },
+  {
+    characters: "老師",
+    phonetic: "Lǎoshī",
+    english: "Teacher",
+  },
+  {
+    characters: "經理",
+    phonetic: "Jīnglǐ",
+    english: "Manager",
+  },
+  {
+    characters: "同事",
+    phonetic: "Tóngshì",
+    english: "Colleague",
+  },
+  {
+    characters: "早餐",
+    phonetic: "Zǎocān",
+    english: "Breakfast",
+  },
+  {
+    characters: "午餐",
+    phonetic: "Wǔcān",
+    english: "Lunch",
+  },
+  {
+    characters: "晚餐",
+    phonetic: "Wǎncān",
+    english: "Dinner",
+  },
+  {
+    characters: "什麼",
+    phonetic: "Shénme",
+    english: "What",
+  },
+  {
+    characters: "喜歡",
+    phonetic: "Xǐhuān",
+    english: "Like",
+  },
+  {
+    characters: "國家",
+    phonetic: "Guójiā",
+    english: "Country",
+  },
+  {
+    characters: "快樂",
+    phonetic: "Kuàilè",
+    english: "Happy",
+  },
+  {
+    characters: "可愛",
+    phonetic: "Kě'ài",
+    english: "Cute",
+  },
+  {
+    characters: "大哥",
+    phonetic: "Dàgē",
+    english: "Big brother",
+  },
+  {
+    characters: "天天",
+    phonetic: "Tiāntiān",
+    english: "Everyday",
+  },
 ];
 
 export default Content;
