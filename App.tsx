52dae7a7f3f791ab56c020095dacb9df60b24052
Sean Smith <sean.smith@tenx.tech>
2019-01-22 00:49:36 +0800

    Add react-native-screens

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import {
+  DefaultTheme,
+  Provider as ReactNativePaperProvider,
+} from "react-native-paper";
+import { useScreens } from "react-native-screens";
 import Sentry from "sentry-expo";
 
 import { COLORS } from "@src/constants/Colors";
 import App from "@src/RootContainer";
+
+useScreens();
 
 Sentry.config(String(process.env.SENTRY_DSN)).install();
 
@@ -20,9 +26,9 @@ const theme = {
 export default class extends React.Component {
   render(): JSX.Element {
     return (
-      <PaperProvider theme={theme}>
+      <ReactNativePaperProvider theme={theme}>
         <App />
-      </PaperProvider>
+      </ReactNativePaperProvider>
     );
   }
 }
