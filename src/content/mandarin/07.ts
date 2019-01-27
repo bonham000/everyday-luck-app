e92894669acf8e51c6bb38b2fa6845e32fc8a8b4
Sean Smith <sean.smith@tenx.tech>
2019-01-27 23:47:26 +0800

    Add content to lesson 07

--- src/content/mandarin/07.ts
+++ src/content/mandarin/07.ts
@@ -9,4 +9,29 @@ export default [
     phonetic: "Wǒ dǒngle",
     english: "I understand",
   },
+  {
+    characters: "別擔心",
+    phonetic: "Bié dānxīn",
+    english: "Do not worry",
+  },
+  {
+    characters: "怎麼了",
+    phonetic: "Zěnme liǎo",
+    english: "What happened?",
+  },
+  {
+    characters: "可以嗎",
+    phonetic: "Kěyǐ ma",
+    english: "May I?",
+  },
+  {
+    characters: "問題",
+    phonetic: "Wèntí",
+    english: "Problem",
+  },
+  {
+    characters: "題",
+    phonetic: "Tí",
+    english: "Question",
+  },
 ];
