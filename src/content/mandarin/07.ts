00c01c4d16d678a6dca894d6887e98218ed64a4a
Sean Smith <sean.smith@tenx.tech>
2019-01-29 00:55:53 +0800

    Adds content to 07

--- src/content/mandarin/07.ts
+++ src/content/mandarin/07.ts
@@ -34,4 +34,74 @@ export default [
     phonetic: "Tí",
     english: "Question",
   },
+  {
+    characters: "拍",
+    phonetic: "Pāi",
+    english: "To pat, clap, slap, take (a photo)",
+  },
+  {
+    characters: "照片",
+    phonetic: "Zhàopiàn",
+    english: "Photo",
+  },
+  {
+    characters: "為什麼我要拍照片",
+    phonetic: "Wèishéme wǒ yào pāi zhàopiàn",
+    english: "Why do I have to take a photo?",
+  },
+  {
+    characters: "我忘了",
+    phonetic: "Wǒ wàngle",
+    english: "I forgot",
+  },
+  {
+    characters: "我不要",
+    phonetic: "Wǒ bùyào",
+    english: "I do not want",
+  },
+  {
+    characters: "我不要告訴你",
+    phonetic: "Wǒ bùyào gàosù nǐ",
+    english: "I do not want to tell you",
+  },
+  {
+    characters: "記憶",
+    phonetic: "Jìyì",
+    english: "Memory",
+  },
+  {
+    characters: "我沒有記憶",
+    phonetic: "Wǒ méiyǒu jìyì",
+    english: "I have no memory",
+  },
+  {
+    characters: "如",
+    phonetic: "Rú",
+    english: "Such as",
+  },
+  {
+    characters: "關係",
+    phonetic: "Guānxì",
+    english: "Relationship",
+  },
+  {
+    characters: "包",
+    phonetic: "Bāo",
+    english: "Package",
+  },
+  {
+    characters: "紅",
+    phonetic: "Hóng",
+    english: "Red",
+  },
+  {
+    characters: "紅包",
+    phonetic: "Hóngbāo",
+    english: "Red packet",
+  },
+  {
+    characters: "許多",
+    phonetic: "Xǔduō",
+    english: "Many",
+  },
 ];
