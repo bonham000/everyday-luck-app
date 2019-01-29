f5ecf63636c70ba3771883bf52684aa133be2b00
Sean Smith <sean.smith@tenx.tech>
2019-01-30 00:12:59 +0800

    Fix bug for handling additional lessons in setting user score state

--- src/content/store.ts
+++ src/content/store.ts
@@ -2,16 +2,27 @@ import { AsyncStorage } from "react-native";
 
 import { getLessonSet } from "@src/content/index.ts";
 import { ScoreStatus } from "@src/GlobalContext";
+import { Lesson } from "./types";
 
 const STORE_KEY = "SCORES";
+
+const LESSON_SET = getLessonSet("Mandarin");
 
 export const getExistingUserScoresAsync = async () => {
   try {
     const result = await AsyncStorage.getItem(STORE_KEY);
-    const parsedResult = JSON.parse(result);
+    let parsedResult = JSON.parse(result);
 
     if (!parsedResult) {
       throw new Error("Data uninitialized");
+    } else {
+      if (parsedResult.length < LESSON_SET.length) {
+        parsedResult = parsedResult.concat(
+          new Array(LESSON_SET.length - parsedResult.length)
+            .fill("")
+            .map(fillLesson),
+        );
+      }
     }
 
     return parsedResult;
@@ -38,9 +49,9 @@ export const resetUserScoresAsync = async () => {
   }
 };
 
-export const initialLessonScoreState: ScoreStatus = getLessonSet(
-  "Mandarin",
-).map(() => ({
+const fillLesson = (item: Lesson) => ({
   mc: false,
   q: false,
-}));
+});
+
+export const initialLessonScoreState: ScoreStatus = LESSON_SET.map(fillLesson);
