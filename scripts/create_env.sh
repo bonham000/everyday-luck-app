0712a2b32c591a1cf0ab66bc56838e59d5dc0bdc
Sean Smith <sean.smith@tenx.tech>
2019-05-02 22:01:06 +0800

    Fix script for creating env.ts file in CI

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -6,5 +6,12 @@ const SENTRY_DSN = \"\";
 const SENTRY_AUTH_TOKEN = \"\";
 const ANDROID_CLIENT_ID = \"\";
 const IOS_CLIENT_ID = \"\";
+const DRAGON_URI = \"\";
 
-export { SENTRY_DSN, SENTRY_AUTH_TOKEN, ANDROID_CLIENT_ID, IOS_CLIENT_ID };" > env.ts
+export {
+  SENTRY_DSN,
+  SENTRY_AUTH_TOKEN,
+  ANDROID_CLIENT_ID,
+  IOS_CLIENT_ID,
+  DRAGON_URI,
+ };" > env.ts
