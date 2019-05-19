791906ae6b97ba40647574006b3028d22c2f3554
Sean Smith <sean.smith@tenx.tech>
2019-05-19 13:30:23 +0800

    WIP - adding translation tool using Google Translation API for converting simplified to traditional

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
 declare module "react-native-confetti";
 declare module "react-native-deck-swiper";
 declare module "react-native-flip-card";
+declare module "@google-cloud/translate";
 
 declare module "*.json" {
   const value: any;
