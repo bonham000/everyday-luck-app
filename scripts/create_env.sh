4088141b82da69f5e305a3e71e5e84aeec946179
Sean Smith <sean.smith@tenx.tech>
2019-05-21 02:53:01 +0800

    Fix create_env script

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -8,4 +8,6 @@ export const ANDROID_CLIENT_ID = \"\";
 export const IOS_CLIENT_ID = \"\";
 export const DRAGON_URI = \"\";
 export const FORVO_API_KEY = \"\";
-export const GOOGLE_TRANSLATE_API_KEY = \"\";" > env.ts
+export const GOOGLE_TRANSLATE_API_KEY = \"\";
+export const PINYIN_CONVERSION_SERVICE_API_KEY = \"asdf769af6dsa896f98as6df9a\";
+" > env.ts
