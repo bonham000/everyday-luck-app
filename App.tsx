83c5d52de1a5b7a24d7236036f4397e428e1e82b
Sean Smith <sean.smith@tenx.tech>
2019-05-23 03:22:40 +0800

    Revert Stargazer setup

--- App.tsx
+++ App.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { Provider as ReactNativePaperProvider } from "react-native-paper";
-import Stargazer from "react-native-stargazer";
+import { useScreens } from "react-native-screens";
+import Sentry from "sentry-expo";
 
+import App from "@src/AppContainer";
+import { COLORS } from "@src/constants/Colors";
 import { APP_THEME } from "@src/constants/Theme";
-import { ROUTES } from "@src/NavigatorConfig";
-import CONFIG from "@src/tools/config";
-import { View } from "react-native";
-import stargazerRoutes from "./tests/StargazerRouteConfig";
 
 /** ========================================================================
- * Stargazer App
+ * Config
+ * =========================================================================
+ */
+
+useScreens();
+
+Sentry.config(String(process.env.SENTRY_DSN)).install();
+
+/** ========================================================================
+ * Mandarin App
  * =========================================================================
  */
 
 export default class extends React.Component {
   render(): JSX.Element {
     return (
-      <View style={{ flex: 1 }}>
-        <ReactNativePaperProvider theme={APP_THEME}>
-          <Stargazer
-            appRouteConfig={ROUTES}
-            routeConfig={stargazerRoutes}
-            stargazerServerUrl={CONFIG.STARGAZER_SERVER_URL}
-          />
-        </ReactNativePaperProvider>
-      </View>
+      <ReactNativePaperProvider theme={APP_THEME}>
+        <App />
+      </ReactNativePaperProvider>
     );
   }
 }
