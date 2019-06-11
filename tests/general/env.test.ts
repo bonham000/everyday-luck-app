d6aed99e2199f749ee3da67cd2dcdc0c7eb4a875
Sean Smith <sean.smith@tenx.tech>
2019-06-12 01:01:22 +0800

    Fix tests

--- tests/general/env.test.ts
+++ tests/general/env.test.ts
@@ -4,18 +4,19 @@ describe("Environment Variables Configuration", () => {
   test("Contains the same keys - if this test fails a key was added. Be sure to update the create_env.sh script!", () => {
     const vars = Object.keys(ENV).sort();
     expect(vars).toMatchInlineSnapshot(`
-Array [
-  "ANDROID_CLIENT_ID",
-  "DRAGON_URI",
-  "FORVO_API_KEY",
-  "GOOGLE_TRANSLATE_API_KEY",
-  "IOS_CLIENT_ID",
-  "PINYIN_CONVERSION_SERVICE_API_KEY",
-  "PINYIN_CONVERSION_SERVICE_URL",
-  "SENTRY_AUTH_TOKEN",
-  "SENTRY_DSN",
-  "STARGAZER_SERVER_URL",
-]
-`);
+      Array [
+        "ANDROID_CLIENT_ID",
+        "ANDROID_STANDALONE_CLIENT_ID",
+        "DRAGON_URI",
+        "FORVO_API_KEY",
+        "GOOGLE_TRANSLATE_API_KEY",
+        "IOS_CLIENT_ID",
+        "PINYIN_CONVERSION_SERVICE_API_KEY",
+        "PINYIN_CONVERSION_SERVICE_URL",
+        "SENTRY_AUTH_TOKEN",
+        "SENTRY_DSN",
+        "STARGAZER_SERVER_URL",
+      ]
+    `);
   });
 });
