8419e2ed77cfefc739c8a952def97b7494d9e573
Sean Smith <sean.smith@tenx.tech>
2019-06-15 04:13:58 +0800

    Fix tests

--- src/components/SideMenuComponent.tsx
+++ src/components/SideMenuComponent.tsx
@@ -44,7 +44,6 @@ class SideMenuComponent extends React.Component<IProps, {}> {
 
   render(): JSX.Element {
     const {
-      user,
       experience,
       languageSetting,
       updateAvailable,
