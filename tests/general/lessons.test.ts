0a2700029fc5ea79c4f73f4add23d924b2e32747
Sean Smith <sean.smith.2009@gmail.com>
2019-12-12 22:47:41 +0800

    Tests

--- tests/general/lessons.test.ts
+++ tests/general/lessons.test.ts
@@ -2,16 +2,7 @@ import HSK_LISTS from "@src/lessons";
 import { APP_DIFFICULTY_SETTING } from "@src/providers/GlobalStateContext";
 import { adjustListContentByDifficultySetting } from "@src/tools/utils";
 
-describe.only("HSK_LISTS content", () => {
-  test("Lesson content doesn't change", () => {
-    const lessons = HSK_LISTS;
-    expect(lessons.length).toMatchInlineSnapshot(`7`);
-    for (const lesson of lessons) {
-      expect(lesson.list).toMatchSnapshot();
-      expect(lesson.content.length).toMatchSnapshot();
-    }
-  });
-
+describe("HSK_LISTS content", () => {
   test("No lesson quiz can have overlapping English words", () => {
     for (const list of HSK_LISTS) {
       const batchedLists = adjustListContentByDifficultySetting(
