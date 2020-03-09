6e8ed501fc5579ab7205d37c4769bcc602bfafef
Sean Smith <sean.smith.2009@gmail.com>
2020-03-09 18:07:38 +0800

    Add instructions for adding words

--- vocabulary/words_list.ts
+++ vocabulary/words_list.ts
@@ -1,3 +1,10 @@
+// Instructions for adding new content:
+// 1. Update the lesson block list and title
+// 2. Add new traditional characters to the words list
+// 3. Run yarn add:list
+// 4. Update src/lessons/index.ts to import the new list
+// 5. Bump the version and redeploy the app
+
 // Metadata for lesson:
 const lesson = {
   list: "8",
