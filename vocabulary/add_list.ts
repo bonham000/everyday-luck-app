d93e21a2820df09f02138206e6bf9f6de38025c7
Sean Smith <sean.smith.2009@gmail.com>
2020-03-11 11:53:43 +0800

    Modify add word script to allow modifying existing word list

--- vocabulary/add_list.ts
+++ vocabulary/add_list.ts
@@ -2,10 +2,23 @@ import fs from "fs";
 
 import { HSKList, TRADITIONAL_CHINESE, Word } from "@src/tools/types";
 import { capitalize, translateWord } from "@src/tools/utils";
-import { lesson, words } from "./words_list";
+import { content, lesson } from "./words_list";
+
+// Flag to modify existing lesson or use new word list
+const USE_EXISTING_LESSON = true;
+
+// Read existing lesson file
+import existingLesson from "@src/lessons/08";
+
+const existingLessonWithContent = {
+  ...existingLesson,
+  content: existingLesson.content.concat(content),
+};
+
+const targetLesson = USE_EXISTING_LESSON ? existingLessonWithContent : lesson;
 
 // Replace the following with the custom word list index:
-const FILE_INDEX_KEY = lesson.list;
+const FILE_INDEX_KEY = targetLesson.list;
 const FILENAME = `src/lessons/${FILE_INDEX_KEY}.ts`;
 
 // Create the file contents
@@ -19,7 +32,7 @@ export default lesson;`;
 // Write the results to a JSON file
 const writeListToJson = (result: ReadonlyArray<Word>) => {
   console.log(`Writing JSON result to file: ${FILENAME}\n`);
-  const list: HSKList = { ...lesson, content: result };
+  const list: HSKList = { ...targetLesson, content: result };
   const data = JSON.stringify(list, null, 2);
   const file = getFileContents(data);
   fs.writeFileSync(FILENAME, file, "utf8");
@@ -27,10 +40,21 @@ const writeListToJson = (result: ReadonlyArray<Word>) => {
 
 // Process the list and fill in the content for each word
 const processListAndTranslateSimplifiedToTraditional = async (
-  list: ReadonlyArray<string>,
+  list: ReadonlyArray<Word>,
 ) => {
   const translated = await Promise.all(
-    list.map(async word => translateWord(word, TRADITIONAL_CHINESE)),
+    list
+      .filter(x => !!x.traditional)
+      .map(async word => {
+        const wordComplete =
+          word.traditional && word.simplified && word.pinyin && word.english;
+
+        if (wordComplete) {
+          return word;
+        } else {
+          return translateWord(word.traditional, TRADITIONAL_CHINESE);
+        }
+      }),
   );
 
   const ordered = translated.map(
@@ -46,11 +70,11 @@ const processListAndTranslateSimplifiedToTraditional = async (
 };
 
 // Run the program with log messages
-const main = async () => {
+const processWordList = async () => {
   console.log(`Processing word list, generating file: ${FILENAME}\n`);
-  await processListAndTranslateSimplifiedToTraditional(words);
+  await processListAndTranslateSimplifiedToTraditional(existingLesson.content);
   console.log("Finished!\n");
 };
 
 // Run it!
-main();
+processWordList();
--- vocabulary/words_list.ts
+++ vocabulary/words_list.ts
@@ -1,3 +1,5 @@
+import { HSKList } from "@src/tools/types";
+
 // Instructions for adding new content:
 // 1. Update the lesson block list and title
 // 2. Add new traditional characters to the words list
@@ -5,98 +7,58 @@
 // 4. Update src/lessons/index.ts to import the new list
 // 5. Bump the version and redeploy the app
 
+// Words to add:
+const words: ReadonlyArray<string> = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+];
+
+export const content = words.map(traditional => ({
+  traditional,
+  simplified: "",
+  pinyin: "",
+  english: "",
+}));
+
 // Metadata for lesson:
-const lesson = {
+export const lesson: HSKList = {
   list: "8",
   locked: false,
   title: "Dictation",
-  content: [],
+  content,
 };
-
-// Words to add:
-const words: ReadonlyArray<string> = [
-  "陳",
-  "月",
-  "美",
-  "李",
-  "明",
-  "華",
-  "王",
-  "開",
-  "文",
-  "你",
-  "來",
-  "是",
-  "小",
-  "姐",
-  "嗎",
-  "接",
-  "我",
-  "們",
-  "這",
-  "先",
-  "生",
-  "好",
-  "姓",
-  "叫",
-  "臺",
-  "灣",
-  "歡",
-  "迎",
-  "請",
-  "問",
-  "的",
-  "謝",
-  "不",
-  "客",
-  "氣",
-  "喝",
-  "茶",
-];
-
-// Export
-export { lesson, words };
-
-// @ts-ignore
-const emptyStrings: ReadonlyArray<""> = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-];
