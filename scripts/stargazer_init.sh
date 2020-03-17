87948c8f3e3545718e33f73fed4147ac45c6924c
Sean Smith <sean.smith.2009@gmail.com>
2020-03-17 12:04:18 +0800

    Remove audio files

--- scripts/stargazer_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Rename StargazerApp.tsx to App.tsx for Stargazer setup
-cp App.tsx App_Temp.tsx
-cp StargazerApp.tsx App.tsx
--- scripts/stargazer_revert.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-# Rename App_Temp.tsx back to App.tsx to revert Stargazer setup
-cp App.tsx StargazerApp.tsx
-cp App_Temp.tsx App.tsx
-rm App_Temp.tsx
