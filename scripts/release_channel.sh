776ae3ec4371278d52ccfb0ca1c51f3627055fe1
Sean Smith <sean.smith.2009@gmail.com>
2020-12-14 14:33:12 +0800

    Remove release_channel script, use default release channel

--- scripts/release_channel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Set the release channel using the current Expo SDK version, e.g. production-33.0.0
-
-export RELEASE_CHANNEL=production-$(cat app.json | jq '.expo.sdkVersion' | sed -e 's/^"//' -e 's/"$//')
-echo "Set release channel to: $RELEASE_CHANNEL"
