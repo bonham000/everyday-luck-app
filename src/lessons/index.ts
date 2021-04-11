0548d8c3ff9234eb72889e39e97a990246447b84
Sean Smith <sean.smith.2009@gmail.com>
2021-04-11 20:14:50 +0800

    Add Lesson 7 Grammar

--- src/lessons/index.ts
+++ src/lessons/index.ts
@@ -11,9 +11,10 @@ import Lesson_11 from "@src/lessons/Contemporary_Chinese_9";
 import Lesson_12 from "@src/lessons/Far_East_1-6";
 import Lesson_13 from "@src/lessons/Contemporary_Chinese_10";
 import Lesson_14 from "@src/lessons/Far_East_7-8";
-import Lesson_15 from "@src/lessons/Sentences_Lesson_8";
+import Lesson_15 from "@src/lessons/Sentences_Lesson_7";
 import Lesson_16 from "@src/lessons/Contemporary_Chinese_11";
 import Lesson_17 from "@src/lessons/Contemporary_Chinese_12";
+import Lesson_18 from "@src/lessons/Sentences_Lesson_8";
 
 import { ContentList, HSKListSet } from "@src/tools/types";
 
@@ -77,6 +78,7 @@ const LISTS: HSKListSet = [
   Lesson_15,
   Lesson_16,
   Lesson_17,
+  Lesson_18,
 ]
   .map(filterEmptyWords)
   .map(setListIndexes);
