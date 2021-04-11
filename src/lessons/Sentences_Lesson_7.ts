6ca14744bef7b1346db7ea9cc546123da7ae6f6a
Sean Smith <sean.smith.2009@gmail.com>
2021-04-11 20:35:37 +0800

    Add lesson 7 grammar

--- src/lessons/Sentences_Lesson_7.ts
+++ src/lessons/Sentences_Lesson_7.ts
@@ -5,7 +5,380 @@ const lesson: ContentList = {
   type: "Sentences",
   locked: false,
   title: "Lessons 7 Grammar",
-  content: [],
+  content: [
+    {
+      traditional: "他和他朋友下午在教室寫書法。",
+      simplified: "他和他朋友下午在教室写书法。",
+      pinyin: "tā hé tā péng yǒu xià wǔ zài jiào shì xiě shū fǎ",
+      english:
+        "He and his friend write calligraphy in the classroom in the afternoon.",
+    },
+    {
+      traditional: "我昨天晚上到我家附近的咖啡點喝咖啡",
+      simplified: "我昨天晚上到我家附近的咖啡点喝咖啡",
+      pinyin:
+        "wǒ zuó tiān wǎn shàng dào wǒ jiā fù jìn dē kā fēi diǎn hē kā fēi",
+      english: "I went to a coffee shop near my house last night for coffee",
+    },
+    {
+      traditional: "我們這個週末圖書館看書。",
+      simplified: "我们这个周末图书馆看书。",
+      pinyin: "wǒ men zhè gè zhōu mò tú shū guǎn kàn shū",
+      english: "We read in the library this weekend.",
+    },
+    {
+      traditional: "你們明天早上十一點到我家來吃牛肉麵。",
+      simplified: "你们明天早上十一点到我家来吃牛肉面。",
+      pinyin:
+        "nǐ men míng tiān zǎo shàng shí yī diǎn dào wǒ jiā lái chī niú ròu miàn",
+      english:
+        "You come to my house for beef noodles at 11 o'clock tomorrow morning.",
+    },
+    {
+      traditional: "我晚上不在家吃飯。",
+      simplified: "我晚上不在家吃饭。",
+      pinyin: "wǒ wǎn shàng bù zài jiā chī fàn",
+      english: "I don't eat at home at night.",
+    },
+    {
+      traditional: "他和他哥哥最近都不來學校上課。",
+      simplified: "他和他哥哥最近都不来学校上课。",
+      pinyin: "tā hé tā gē ge zuì jìn dōu bù lái xué xiào shàng kè",
+      english: "He and his brother have not come to school recently.",
+    },
+    {
+      traditional: "他們這個週末不去山上看風景。",
+      simplified: "他们这个周末不去山上看风景。",
+      pinyin: "tā men zhè gè zhōu mò bù qù shān shàng kàn fēng jǐng",
+      english:
+        "They won't go to the mountains to see the scenery this weekend.",
+    },
+    {
+      traditional: "你下午要不要來學校打籃球？",
+      simplified: "你下午要不要来学校打篮球？",
+      pinyin: "nǐ xià wǔ yào bú yào lái xué xiào dǎ lán qiú",
+      english:
+        "Would you like to come to school to play basketball this afternoon?",
+    },
+    {
+      traditional: "你們現在在我家附近的商店買手機嗎？",
+      simplified: "你们现在在我家附近的商店买手机吗？",
+      pinyin: "nǐ men xiàn zài zài wǒ jiā fù jìn dē shāng diàn mǎi shǒu jī ma",
+      english: "Do you buy mobile phones in a store near my house now?",
+    },
+    {
+      traditional: "你朋友晚上幾點去KTV唱歌？",
+      simplified: "你朋友晚上几点去KTV唱歌？",
+      pinyin: "nǐ péng yǒu wǎn shàng jǐ diǎn qù chàng gē",
+      english: "What time does your friend go to ktv to sing at night?",
+    },
+    {
+      traditional: "他們什麼時候到花蓮看籃球比賽？",
+      simplified: "他们什么时候到花莲看篮球比赛？",
+      pinyin: "tā men shén me shí hòu dào huā lián kàn lán qiú bǐ sài",
+      english: "When will they go to hualien to watch a basketball game?",
+    },
+    {
+      traditional: "你和你妹妹明天早上要去哪裡看電影？",
+      simplified: "你和你妹妹明天早上要去哪里看电影？",
+      pinyin: "nǐ hé nǐ mèi mei míng tiān zǎo shàng yào qù nǎ lǐ kàn diàn yǐng",
+      english:
+        "Where are you and your sister going to watch a movie tomorrow morning?",
+    },
+    {
+      traditional: "我從早上十點二十分到下午一點十分有中文課。",
+      simplified: "我从早上十点二十分到下午一点十分有中文课。",
+      pinyin:
+        "wǒ cóng zǎo shàng shí diǎn èr shí fēn dào xià wǔ yī diǎn shí fēn yǒu zhōng wén kè",
+      english:
+        "I have chinese lessons from 10:20 in the morning to 1:10 in the afternoon.",
+    },
+    {
+      traditional: "我今天從早上到晚上。",
+      simplified: "我今天从早上到晚上。",
+      pinyin: "wǒ jīn tiān cóng zǎo shàng dào wǎn shàng",
+      english: "I am from morning to night today.",
+    },
+    {
+      traditional: "從我家到那個游泳池有一點遠。",
+      simplified: "从我家到那个游泳池有一点远。",
+      pinyin: "cóng wǒ jiā dào nà gè yóu yǒng chí yǒu yī diǎn yuǎn",
+      english: "It's a little far from my house to the swimming pool.",
+    },
+    {
+      traditional: "我從我朋友嫁到這個地方來。",
+      simplified: "我从我朋友嫁到这个地方来。",
+      pinyin: "wǒ cóng wǒ péng yǒu jià dào zhè gè dì fāng lái",
+      english: "I came to this place from the marriage of my friend.",
+    },
+    {
+      traditional: "他從圖書館到那家餐廳去吃飯。",
+      simplified: "他从图书馆到那家餐厅去吃饭。",
+      pinyin: "tā cóng tú shū guǎn dào nà jiā cān tīng qù chī fàn",
+      english: "He went from the library to the restaurant to eat.",
+    },
+    {
+      traditional: "我們的書法課不是從九點到十一點。",
+      simplified: "我们的书法课不是从九点到十一点。",
+      pinyin: "wǒ men dē shū fǎ kè bú shì cóng jiǔ diǎn dào shí yī diǎn",
+      english: "Our calligraphy class is not from nine to eleven.",
+    },
+    {
+      traditional: "陳先生的媽媽昨天不是從早上到晚上都很忙。",
+      simplified: "陈先生的妈妈昨天不是从早上到晚上都很忙。",
+      pinyin:
+        "chén xiān shēng dē mā mā zuó tiān bú shì cóng zǎo shàng dào wǎn shàng dōu hěn máng",
+      english:
+        "Mr. chen's mother was not busy from morning to night yesterday.",
+    },
+    {
+      traditional:
+        "我身時候可以去你家？我今天從早上到晚上都沒有空，可是明天可以。",
+      simplified:
+        "我身时候可以去你家？我今天从早上到晚上都没有空，可是明天可以。",
+      pinyin:
+        "wǒ shēn shí hòu kě yǐ qù nǐ jiā wǒ jīn tiān cóng zǎo shàng dào wǎn shàng dōu méi yǒu kòng kě shì míng tiān kě yǐ",
+      english:
+        "Can i go to your house when i am? i have no time from morning to night today, but tomorrow will be fine.",
+    },
+    {
+      traditional: "從這棟大樓到那家KTV不遠。",
+      simplified: "从这栋大楼到那家KTV不远。",
+      pinyin: "cóng zhè dòng dà lóu dào nà jiā bù yuǎn",
+      english: "It’s not far from this building to that ktv.",
+    },
+    {
+      traditional: "我不想從學校到哪裡去，想從我家去。",
+      simplified: "我不想从学校到哪里去，想从我家去。",
+      pinyin: "wǒ bù xiǎng cóng xué xiào dào nǎ lǐ qù xiǎng cóng wǒ jiā qù",
+      english:
+        "I don't want to go anywhere from school, i want to go from my home.",
+    },
+    {
+      traditional: "你們老師後天從早上到下午都有空嗎？",
+      simplified: "你们老师后天从早上到下午都有空吗？",
+      pinyin:
+        "nǐ men lǎo shī hòu tiān cóng zǎo shàng dào xià wǔ dōu yǒu kòng ma",
+      english:
+        "Is your teacher free from morning to afternoon the day after tomorrow?",
+    },
+    {
+      traditional: "請問從圖書館到你們宿舍遠不遠？",
+      simplified: "请问从图书馆到你们宿舍远不远？",
+      pinyin: "qǐng wèn cóng tú shū guǎn dào nǐ men sù shè yuǎn bù yuǎn",
+      english: "Is it far from the library to your dormitory?",
+    },
+    {
+      traditional: "他們想從宿舍還是圖書館到教室去上課？",
+      simplified: "他们想从宿舍还是图书馆到教室去上课？",
+      pinyin:
+        "tā men xiǎng cóng sù shè hái shì tú shū guǎn dào jiào shì qù shàng kè",
+      english:
+        "Do they want to go to class from the dormitory or the library to the classroom?",
+    },
+    {
+      traditional: "林老師在上課。",
+      simplified: "林老师在上课。",
+      pinyin: "lín lǎo shī zài shàng kè",
+      english: "Teacher lin is in class.",
+    },
+    {
+      traditional: "你看！陳先生在唱歌。",
+      simplified: "你看！陈先生在唱歌。",
+      pinyin: "nǐ kàn chén xiān shēng zài chàng gē",
+      english: "Look! mr. chen is singing.",
+    },
+    {
+      traditional: "昨天下午五點我在做飯。",
+      simplified: "昨天下午五点我在做饭。",
+      pinyin: "zuó tiān xià wǔ wǔ diǎn wǒ zài zuò fàn",
+      english: "I was cooking at five yesterday afternoon.",
+    },
+    {
+      traditional: "他不是在看書。他在看籃球比賽。",
+      simplified: "他不是在看书。他在看篮球比赛。",
+      pinyin: "tā bú shì zài kàn shū tā zài kàn lán qiú bǐ sài",
+      english: "He is not reading. he is watching a basketball game.",
+    },
+    {
+      traditional: "我不是在照相。我的手機不能照相。",
+      simplified: "我不是在照相。我的手机不能照相。",
+      pinyin: "wǒ bú shì zài zhào xiàng wǒ dē shǒu jī bù néng zhào xiàng",
+      english: "I'm not taking pictures. my phone cannot take pictures.",
+    },
+    {
+      traditional: "你們在喝什麼？",
+      simplified: "你们在喝什么？",
+      pinyin: "nǐ men zài hē shén me",
+      english: "What are you drinking?",
+    },
+    {
+      traditional: "他們老師在上課嗎？",
+      simplified: "他们老师在上课吗？",
+      pinyin: "tā men lǎo shī zài shàng kè ma",
+      english: "Is their teacher in class?",
+    },
+    {
+      traditional: "他們在打籃球嗎？",
+      simplified: "他们在打篮球吗？",
+      pinyin: "tā men zài dǎ lán qiú ma",
+      english: "Are they playing basketball?",
+    },
+    {
+      traditional: "他妹妹天都有空。",
+      simplified: "他妹妹天都有空。",
+      pinyin: "tā mèi mei tiān dū yǒu kòng",
+      english: "His sister is free every day.",
+    },
+    {
+      traditional: "他朋友每個週末都去學校附近的游泳池游泳。",
+      simplified: "他朋友每个周末都去学校附近的游泳池游泳。",
+      pinyin:
+        "tā péng yǒu měi gè zhōu mò dōu qù xué xiào fù jìn dē yóu yǒng chí yóu yǒng",
+      english:
+        "His friend goes to the swimming pool near the school to swim every weekend.",
+    },
+    {
+      traditional: "每一棟大樓都可以上網。",
+      simplified: "每一栋大楼都可以上网。",
+      pinyin: "měi yī dòng dà lóu dōu kě yǐ shàng wǎng",
+      english: "Every building has internet access.",
+    },
+    {
+      traditional: "他家人，每個人都會做甜點。",
+      simplified: "他家人，每个人都会做甜点。",
+      pinyin: "tā jiā rén měi gè rén dōu huì zuò tián diǎn",
+      english: "Everyone in his family makes desserts.",
+    },
+    {
+      traditional: "他每天都不忙。",
+      simplified: "他每天都不忙。",
+      pinyin: "tā měi tiān dū bù máng",
+      english: "He is not busy every day.",
+    },
+    {
+      traditional: "我媽媽每個週末都沒空。",
+      simplified: "我妈妈每个周末都没空。",
+      pinyin: "wǒ mā mā měi gè zhōu mò dōu méi kōng",
+      english: "My mother is not free every weekend.",
+    },
+    {
+      traditional: "這家商店，每支手機都不便宜。",
+      simplified: "这家商店，每支手机都不便宜。",
+      pinyin: "zhè jiā shāng diàn měi zhī shǒu jī dōu bù pián yí",
+      english: "In this shop, every cell phone is not cheap.",
+    },
+    {
+      traditional: "他朋友不是每天都去看電影。",
+      simplified: "他朋友不是每天都去看电影。",
+      pinyin: "tā péng yǒu bú shì měi tiān dū qù kàn diàn yǐng",
+      english: "His friend doesn't go to the movies every day.",
+    },
+    {
+      traditional: "我們不是每天都有書法課。",
+      simplified: "我们不是每天都有书法课。",
+      pinyin: "wǒ men bú shì měi tiān dū yǒu shū fǎ kè",
+      english: "We don’t have calligraphy lessons every day.",
+    },
+    {
+      traditional: "他的兄弟姊妹不是每個人都喜歡打球。",
+      simplified: "他的兄弟姊妹不是每个人都喜欢打球。",
+      pinyin: "tā dē xiōng dì zǐ mèi bú shì měi gè rén dōu xǐ huān dǎ qiú",
+      english: "Not everyone of his siblings likes to play ball.",
+    },
+    {
+      traditional: "他每個週末都去哪裡運動？",
+      simplified: "他每个周末都去哪里运动？",
+      pinyin: "tā měi gè zhōu mò dōu qù nǎ lǐ yùn dòng",
+      english: "Where does he go to exercise every weekend?",
+    },
+    {
+      traditional: "你爸爸每天都在家吃晚飯嗎？",
+      simplified: "你爸爸每天都在家吃晚饭吗？",
+      pinyin: "nǐ bà bà měi tiān dū zài jiā chī wǎn fàn ma",
+      english: "Does your father eat dinner at home every day?",
+    },
+    {
+      traditional: "他的照片，每張都很好看嗎？",
+      simplified: "他的照片，每张都很好看吗？",
+      pinyin: "tā dē zhào piān měi zhāng dōu hěn hǎo kàn ma",
+      english: "Are each of his photos pretty good-looking?",
+    },
+    {
+      traditional: "我媽媽說，你們可以來我家打籃球。",
+      simplified: "我妈妈说，你们可以来我家打篮球。",
+      pinyin: "wǒ mā mā shuō nǐ men kě yǐ lái wǒ jiā dǎ lán qiú",
+      english: "My mother said, you can come to my house to play basketball.",
+    },
+    {
+      traditional: "爸爸說，我可以買這支手機。",
+      simplified: "爸爸说，我可以买这支手机。",
+      pinyin: "bà bà shuō wǒ kě yǐ mǎi zhè zhī shǒu jī",
+      english: "Dad said, i can buy this phone.",
+    },
+    {
+      traditional: "我叫安同，你可以教我小同。",
+      simplified: "我叫安同，你可以教我小同。",
+      pinyin: "wǒ jiào ān tóng nǐ kě yǐ jiào wǒ xiǎo tóng",
+      english: "My name is antong, you can teach me xiaotong.",
+    },
+    {
+      traditional: "媽媽說，我不可以買這支手機。",
+      simplified: "妈妈说，我不可以买这支手机。",
+      pinyin: "mā mā shuō wǒ bù kě yǐ mǎi zhè zhī shǒu jī",
+      english: "Mom said, i can’t buy this phone.",
+    },
+    {
+      traditional: "你不可以在圖書館裡面吃東西。",
+      simplified: "你不可以在图书馆里面吃东西。",
+      pinyin: "nǐ bù kě yǐ zài tú shū guǎn lǐ miàn chī dōng xī",
+      english: "You cannot eat in the library.",
+    },
+    {
+      traditional: "先生，對不起，你不可以在這裡照相。",
+      simplified: "先生，对不起，你不可以在这里照相。",
+      pinyin: "xiān shēng duì bù qǐ nǐ bù kě yǐ zài zhè lǐ zhào xiàng",
+      english: "Sorry, sir, you can’t take pictures here.",
+    },
+    {
+      traditional: "我可以不可以去看你們的籃球比賽？沒問題！",
+      simplified: "我可以不可以去看你们的篮球比赛？没问题！",
+      pinyin: "wǒ kě yǐ bù kě yǐ qù kàn nǐ men dē lán qiú bǐ sài méi wèn tí",
+      english: "Can i go to your basketball game? no problem!",
+    },
+    {
+      traditional: "我想吃你的包子，可以嗎？可以啊。",
+      simplified: "我想吃你的包子，可以吗？可以啊。",
+      pinyin: "wǒ xiǎng chī nǐ dē bāo zi kě yǐ ma kě yǐ a",
+      english: "I want to eat your buns, can i? sure.",
+    },
+    {
+      traditional: "這是你的書嗎？我可以看看嗎？對不起，那不是我的書。",
+      simplified: "这是你的书吗？我可以看看吗？对不起，那不是我的书。",
+      pinyin:
+        "zhè shì nǐ dē shū ma wǒ kě yǐ kàn kàn ma duì bù qǐ nà bú shì wǒ dē shū",
+      english:
+        "Is this your book? may i take a look at it? sorry, that is not my book.",
+    },
+    {
+      traditional: "你可不可以明天來？",
+      simplified: "你可不可以明天来？",
+      pinyin: "nǐ kě bù kě yǐ míng tiān lái",
+      english: "Can you come tomorrow?",
+    },
+    {
+      traditional: "我可以不可以買一支新手機？",
+      simplified: "我可以不可以买一支新手机？",
+      pinyin: "wǒ kě yǐ bù kě yǐ mǎi yī zhī xīn shǒu jī",
+      english: "Can i buy a new phone?",
+    },
+    {
+      traditional: "媽媽，我可不可以買這支手機？可以。",
+      simplified: "妈妈，我可不可以买这支手机？可以。",
+      pinyin: "mā mā wǒ kě bù kě yǐ mǎi zhè zhī shǒu jī kě yǐ",
+      english: "Mom, can i buy this phone? can.",
+    },
+  ],
 };
 
 export default lesson;
