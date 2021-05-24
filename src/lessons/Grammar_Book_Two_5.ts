5704ef0d5bab9710451732d5ee6be2585321afe7
Sean Smith <sean.smith.2009@gmail.com>
2021-05-24 13:18:33 +0800

    Add Book 2 Lesson 5 content and grammar

--- src/lessons/Grammar_Book_Two_5.ts
+++ src/lessons/Grammar_Book_Two_5.ts
@@ -5,7 +5,346 @@ const lesson: ContentList = {
   type: "Grammar",
   locked: false,
   title: "Book 2 Lesson 5",
-  content: [],
+  content: [
+    {
+      traditional: "現在時間還早，學生都沒到。",
+      simplified: "现在时间还早，学生都没到。",
+      pinyin: "xiàn zài shí jiān hái zǎo xué shēng dōu méi dào",
+      english: "It's still early, and the students haven't arrived yet.",
+    },
+    {
+      traditional: "來的客人還太少，喜宴還不能開始。",
+      simplified: "来的客人还太少，喜宴还不能开始。",
+      pinyin: "lái dē kè rén hái tài shǎo xǐ yàn hái bù néng kāi shǐ",
+      english:
+        "There are still too few guests, and the wedding banquet cannot start yet.",
+    },
+    {
+      traditional: "現在天氣還太冷，不可以去游泳。",
+      simplified: "现在天气还太冷，不可以去游泳。",
+      pinyin: "xiàn zài tiān qì hái tài lěng bù kě yǐ qù yóu yǒng",
+      english: "The weather is still too cold to go swimming.",
+    },
+    {
+      traditional: "我的錢還不夠，不能付學費。",
+      simplified: "我的钱还不够，不能付学费。",
+      pinyin: "wǒ dē qián hái bù gòu bù néng fù xué fèi",
+      english: "My money is not enough to pay the tuition.",
+    },
+    {
+      traditional: "現在天氣還不熱，我們去東部旅行吧！",
+      simplified: "现在天气还不热，我们去东部旅行吧！",
+      pinyin: "xiàn zài tiān qì hái bù rè wǒ men qù dōng bù lǚ xíng ba",
+      english: "It's not hot yet, let's travel to the east!",
+    },
+    {
+      traditional: "他剛回國，我工作還不順利。",
+      simplified: "他刚回国，我工作还不顺利。",
+      pinyin: "tā gāng huí guó wǒ gōng zuò hái bù shùn lì",
+      english:
+        "He has just returned to china, and my work is still not going well.",
+    },
+    {
+      traditional: "你的錢是不是還不夠租套房？",
+      simplified: "你的钱是不是还不够租套房？",
+      pinyin: "nǐ dē qián shì bú shì hái bù gòu zū tào fáng",
+      english: "Is your money not enough to rent an apartment?",
+    },
+    {
+      traditional: "你喝得這麼慢，牛肉湯還很熱嗎？",
+      simplified: "你喝得这么慢，牛肉汤还很热吗？",
+      pinyin: "nǐ hē dé zhè me màn niú ròu tāng hái hěn rè ma",
+      english: "You drink so slowly, is the beef soup still hot?",
+    },
+    {
+      traditional: "十二點的飛機，現在六點，去機場是不是還太早？",
+      simplified: "十二点的飞机，现在六点，去机场是不是还太早？",
+      pinyin:
+        "shí èr diǎn dē fēi jī xiàn zài liù diǎn qù jī chǎng shì bú shì hái tài zǎo",
+      english:
+        "The plane at twelve o'clock, now six o'clock, is it too early to get to the airport?",
+    },
+    {
+      traditional: "我們還沒決定，你有什麼建議？",
+      simplified: "我们还没决定，你有什么建议？",
+      pinyin: "wǒ men hái méi jué dìng nǐ yǒu shén me jiàn yì",
+      english: "We haven't decided yet. what suggestions do you have?",
+    },
+    {
+      traditional: "可是我說話，還說得不夠流利。",
+      simplified: "可是我说话，还说得不够流利。",
+      pinyin: "kě shì wǒ shuō huà hái shuō dé bù gòu liú lì",
+      english: "But i still don't speak fluently enough.",
+    },
+    {
+      traditional: "菜還很多，你要多吃一點。",
+      simplified: "菜还很多，你要多吃一点。",
+      pinyin: "cài hái hěn duō nǐ yào duō chī yī diǎn",
+      english: "There are still a lot of dishes, you have to eat more.",
+    },
+    {
+      traditional: "這個工作得薪水還不夠付小孩的學費，他的再找另外一個工作。",
+      simplified: "这个工作得薪水还不够付小孩的学费，他的再找另外一个工作。",
+      pinyin:
+        "zhè gè gōng zuò dé xīn shuǐ hái bù gòu fù xiǎo hái dē xué fèi tā dē zài zhǎo lìng wài yī gè gōng zuò",
+      english:
+        "The salary for this job is not enough to pay the child's tuition, so he will find another job.",
+    },
+    {
+      traditional: "昨天晚上肚子很不舒服，還肚子好幾次。",
+      simplified: "昨天晚上肚子很不舒服，还肚子好几次。",
+      pinyin: "zuó tiān wǎn shàng dǔ zi hěn bù shū fu hái dǔ zi hǎo jǐ cì",
+      english:
+        "My stomach was very uncomfortable last night, and i got my stomach several times.",
+    },
+    {
+      traditional: "下了課要寫作業，還要準備第二天的課。",
+      simplified: "下了课要写作业，还要准备第二天的课。",
+      pinyin: "xià le kè yào xiě zuò yè hái yào zhǔn bèi dì èr tiān dē kè",
+      english:
+        "I have to write homework after class and prepare for the next day's class.",
+    },
+    {
+      traditional: "他笑著跟客人說話。",
+      simplified: "他笑着跟客人说话。",
+      pinyin: "tā xiào zhe gēn kè rén shuō huà",
+      english: "He smiled and talked to the guests.",
+    },
+    {
+      traditional: "站這吃飯不好吧！",
+      simplified: "站这吃饭不好吧！",
+      pinyin: "zhàn zhè chī fàn bù hǎo ba",
+      english: "It's not good to stand here to eat!",
+    },
+    {
+      traditional: "哪裡得路很複雜，你還是帶著地圖去吧！",
+      simplified: "哪里得路很复杂，你还是带着地图去吧！",
+      pinyin: "nǎ lǐ dé lù hěn fù zá nǐ hái shì dài zhe dì tú qù ba",
+      english:
+        "Where the road is very complicated, you should take the map with you!",
+    },
+    {
+      traditional: "李老師不是站這上課，他坐這上課。",
+      simplified: "李老师不是站这上课，他坐这上课。",
+      pinyin: "lǐ lǎo shī bú shì zhàn zhè shàng kè tā zuò zhè shàng kè",
+      english:
+        "Teacher li doesn't stand here for class, he sits here for class.",
+    },
+    {
+      traditional: "爸爸說別看著電視吃飯。",
+      simplified: "爸爸说别看着电视吃饭。",
+      pinyin: "bà bà shuō bié kàn zhe diàn shì chī fàn",
+      english: "Dad said not to watch tv and eat.",
+    },
+    {
+      traditional: "你站了多久了？怎麼不坐著等他？",
+      simplified: "你站了多久了？怎么不坐着等他？",
+      pinyin: "nǐ zhàn le duō jiǔ le zěn me bù zuò zhe děng tā",
+      english:
+        "How long have you been standing? why don't you just sit and wait for him?",
+    },
+    {
+      traditional: "安同是不是聽著音樂騎機車？",
+      simplified: "安同是不是听着音乐骑机车？",
+      pinyin: "ān tóng shì bú shì tīng zhe yīn lè qí jī chē",
+      english: "Does an tong ride a motorcycle while listening to music?",
+    },
+    {
+      traditional: "他是不是帶著兩個大背包去旅行？",
+      simplified: "他是不是带着两个大背包去旅行？",
+      pinyin: "tā shì bú shì dài zhe liǎng gè dà bēi bāo qù lǚ xíng",
+      english: "Does he travel with two big backpacks?",
+    },
+    {
+      traditional: "他是不是拿著雨傘下樓了？",
+      simplified: "他是不是拿着雨伞下楼了？",
+      pinyin: "tā shì bú shì ná zhe yǔ sǎn xià lóu le",
+      english: "Did he go downstairs with an umbrella?",
+    },
+    {
+      traditional: "他忙著找工作。",
+      simplified: "他忙着找工作。",
+      pinyin: "tā máng zhe zhǎo gōng zuò",
+      english: "He is busy looking for work.",
+    },
+    {
+      traditional: "陳小姐急著準備找工作的履歷表。",
+      simplified: "陈小姐急着准备找工作的履历表。",
+      pinyin: "chén xiǎo jiě jí zhe zhǔn bèi zhǎo gōng zuò dē lǚ lì biǎo",
+      english: "Miss chen is anxious to prepare a resume for job hunting.",
+    },
+    {
+      traditional: "媽媽忙著給大家做飯。",
+      simplified: "妈妈忙着给大家做饭。",
+      pinyin: "mā mā máng zhe gěi dà jiā zuò fàn",
+      english: "Mother is busy cooking for everyone.",
+    },
+    {
+      traditional: "我帶你們去你們的座位吧！",
+      simplified: "我带你们去你们的座位吧！",
+      pinyin: "wǒ dài nǐ men qù nǐ men dē zuò wèi ba",
+      english: "I will take you to your seats!",
+    },
+    {
+      traditional: "安同送美拎回宿舍。",
+      simplified: "安同送美拎回宿舍。",
+      pinyin: "ān tóng sòng měi līn huí sù shè",
+      english: "An tong sent mei back to the dormitory.",
+    },
+    {
+      traditional: "我跟弟弟去機場接朋友。",
+      simplified: "我跟弟弟去机场接朋友。",
+      pinyin: "wǒ gēn dì di qù jī chǎng jiē péng yǒu",
+      english: "My brother and i went to the airport to meet friends.",
+    },
+    {
+      traditional: "我還有事，我不送他回家了。",
+      simplified: "我还有事，我不送他回家了。",
+      pinyin: "wǒ hái yǒu shì wǒ bù sòng tā huí jiā le",
+      english: "I have something else, i won't send him home anymore.",
+    },
+    {
+      traditional: "他沒送我去郵局，我是自己去的。",
+      simplified: "他没送我去邮局，我是自己去的。",
+      pinyin: "tā méi sòng wǒ qù yóu jú wǒ shì zì jǐ qù dē",
+      english: "He didn't send me to the post office, i did it by myself.",
+    },
+    {
+      traditional: "我是大人了，你別送我去學校了。",
+      simplified: "我是大人了，你别送我去学校了。",
+      pinyin: "wǒ shì dà rén le nǐ bié sòng wǒ qù xué xiào le",
+      english: "I'm an adult, don't send me to school.",
+    },
+    {
+      traditional: "你要不要坐火車到台東，在搭船到綠島？",
+      simplified: "你要不要坐火车到台东，在搭船到绿岛？",
+      pinyin: "nǐ yào bú yào zuò huǒ chē dào tái dōng zài dā chuán dào lǜ dǎo",
+      english:
+        "Would you like to take a train to taitung, or take a boat to green island?",
+    },
+    {
+      traditional: "你是不是捷運去貓空喝茶？",
+      simplified: "你是不是捷运去猫空喝茶？",
+      pinyin: "nǐ shì bú shì jié yùn qù māo kōng hē chá",
+      english: "Are you mrt going to maokong for tea?",
+    },
+    {
+      traditional: "王老師明天帶不帶我們去看電腦展？",
+      simplified: "王老师明天带不带我们去看电脑展？",
+      pinyin: "wáng lǎo shī míng tiān dài bù dài wǒ men qù kàn diàn nǎo zhǎn",
+      english: "Teacher wang, will you take us to the computer show tomorrow?",
+    },
+    {
+      traditional: "等客人都回去了，喜宴結束了。",
+      simplified: "等客人都回去了，喜宴结束了。",
+      pinyin: "děng kè rén dōu huí qù le xǐ yàn jié shù le",
+      english: "When the guests have gone back, the wedding banquet is over.",
+    },
+    {
+      traditional: "等夏天到了，去海邊的人就多了。",
+      simplified: "等夏天到了，去海边的人就多了。",
+      pinyin: "děng xià tiān dào le qù hǎi biān dē rén jiù duō le",
+      english:
+        "When summer is here, there will be more people going to the beach.",
+    },
+    {
+      traditional: "等你準備找工作的時候，我就會給你一些建議。",
+      simplified: "等你准备找工作的时候，我就会给你一些建议。",
+      pinyin:
+        "děng nǐ zhǔn bèi zhǎo gōng zuò dē shí hòu wǒ jiù huì gěi nǐ yī xiē jiàn yì",
+      english:
+        "When you are ready to find a job, i will give you some suggestions.",
+    },
+    {
+      traditional: "不等足球比賽結束，加油的學生就走了。",
+      simplified: "不等足球比赛结束，加油的学生就走了。",
+      pinyin: "bù děng zú qiú bǐ sài jié shù jiā yóu dē xué shēng jiù zǒu le",
+      english:
+        "Before the football match was over, the cheering students left.",
+    },
+    {
+      traditional: "看到桌上有吃的，有喝的，不等明華回來，大家就先吃了。",
+      simplified: "看到桌上有吃的，有喝的，不等明华回来，大家就先吃了。",
+      pinyin:
+        "kàn dào zhuō shàng yǒu chī dē yǒu hē dē bù děng míng huá huí lái dà jiā jiù xiān chī le",
+      english:
+        "Seeing that there was something to eat and drink on the table, everyone ate first before minghua came back.",
+    },
+    {
+      traditional: "他常常練習沒學過的字，所以不等老師教他，他就都會了。",
+      simplified: "他常常练习没学过的字，所以不等老师教他，他就都会了。",
+      pinyin:
+        "tā cháng cháng liàn xí méi xué guò dē zì suǒ yǐ bù děng lǎo shī jiào tā tā jiù dōu huì le",
+      english:
+        "He often practises words he hasn't learned, so he can do it without waiting for the teacher to teach him.",
+    },
+    {
+      traditional: "是不是等我結婚的時候，你就會來參加我的婚禮？",
+      simplified: "是不是等我结婚的时候，你就会来参加我的婚礼？",
+      pinyin:
+        "shì bú shì děng wǒ jié hūn dē shí hòu nǐ jiù huì lái cān jiā wǒ dē hūn lǐ",
+      english: "Will you come to my wedding when i get married?",
+    },
+    {
+      traditional: "等早上九點以後，交通是不是就會比較好？",
+      simplified: "等早上九点以后，交通是不是就会比较好？",
+      pinyin:
+        "děng zǎo shàng jiǔ diǎn yǐ hòu jiāo tōng shì bú shì jiù huì bǐ jiào hǎo",
+      english: "Will the traffic be better after nine o'clock in the morning?",
+    },
+    {
+      traditional: "是不是等美美從東部旅行回來，我們就請她介紹介紹那裡的風景？",
+      simplified: "是不是等美美从东部旅行回来，我们就请她介绍介绍那里的风景？",
+      pinyin:
+        "shì bú shì děng měi měi cóng dōng bù lǚ xíng huí lái wǒ men jiù qǐng tā jiè shào jiè shào nà lǐ dē fēng jǐng",
+      english:
+        "Isn’t it true that when meimei returns from a trip to the east, we will ask her to introduce the scenery there?",
+    },
+    {
+      traditional: "大家吃著吃著，新郎，新娘和他們的父母就來敬酒了。",
+      simplified: "大家吃着吃着，新郎，新娘和他们的父母就来敬酒了。",
+      pinyin:
+        "dà jiā chī zhe chī zhe xīn láng xīn niáng hé tā men dē fù mǔ jiù lái jìng jiǔ le",
+      english:
+        "Everyone ate and ate, and the bridegroom, bride and their parents came to toast.",
+    },
+    {
+      traditional: "我們等著等著，喜宴就開始了。",
+      simplified: "我们等着等着，喜宴就开始了。",
+      pinyin: "wǒ men děng zhe děng zhe xǐ yàn jiù kāi shǐ le",
+      english: "We waited and waited, and the wedding banquet began.",
+    },
+    {
+      traditional: "我小時候每天看媽媽做飯，看著看著，就會了。",
+      simplified: "我小时候每天看妈妈做饭，看着看着，就会了。",
+      pinyin:
+        "wǒ xiǎo shí hòu měi tiān kàn mā mā zuò fàn kàn zhe kàn zhe jiù huì le",
+      english:
+        "When i was a child, i watched my mother cook every day, watched and watched, it would be done.",
+    },
+    {
+      traditional: "這個歌，我聽著聽著，就會唱了。",
+      simplified: "这个歌，我听着听着，就会唱了。",
+      pinyin: "zhè gè gē wǒ tīng zhe tīng zhe jiù huì chàng le",
+      english: "This song, i will sing as i listen to it.",
+    },
+    {
+      traditional: "電視在介紹日本的風景，他看著看著，就像家了。",
+      simplified: "电视在介绍日本的风景，他看着看着，就像家了。",
+      pinyin:
+        "diàn shì zài jiè shào rì běn dē fēng jǐng tā kàn zhe kàn zhe jiù xiàng jiā le",
+      english:
+        "The tv was introducing the scenery of japan, and he looked at it, it was like home.",
+    },
+    {
+      traditional: "安同在捷運上看事，看著看著，就空了換車了。",
+      simplified: "安同在捷运上看事，看着看着，就空了换车了。",
+      pinyin:
+        "ān tóng zài jié yùn shàng kàn shì kàn zhe kàn zhe jiù kōng le huàn chē le",
+      english:
+        "An tong was watching things on the mrt, and then he was left to change trains.",
+    },
+  ],
 };
 
 export default lesson;
