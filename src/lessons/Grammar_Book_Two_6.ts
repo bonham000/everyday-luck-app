1994d0393eb3c9768f7fc1fb01e2129c1649de15
Sean Smith <sean.smith.2009@gmail.com>
2021-06-03 11:00:59 +0800

    Add Lesson 6 vocabulary and grammar

--- src/lessons/Grammar_Book_Two_6.ts
+++ src/lessons/Grammar_Book_Two_6.ts
@@ -5,7 +5,427 @@ const lesson: ContentList = {
   type: "Grammar",
   locked: false,
   title: "Book 2 Lesson 6",
-  content: [],
+  content: [
+    {
+      traditional: "他在踢足球，踢著踢著就踢到學校外面了。",
+      simplified: "他在踢足球，踢着踢着就踢到学校外面了。",
+      pinyin: "tā zài tī zú qiú tī zhe tī zhe jiù tī dào xué xiào wài miàn le",
+      english: "He was playing football, kicked and kicked out of the school.",
+    },
+    {
+      traditional: "馬安同上星期從他被騎機車騎到花蓮。",
+      simplified: "马安同上星期从他被骑机车骑到花莲。",
+      pinyin: "mǎ ān tóng shàng xīng qī cóng tā bèi qí jī chē qí dào huā lián",
+      english:
+        "Ma antong was ridden from him to hualien on a motorcycle last week.",
+    },
+    {
+      traditional: "這個蛋糕，我打算拿到學校請同學吃。",
+      simplified: "这个蛋糕，我打算拿到学校请同学吃。",
+      pinyin: "zhè gè dàn gāo wǒ dǎ suàn ná dào xué xiào qǐng tóng xué chī",
+      english: "I plan to take this cake to school for my classmates to eat.",
+    },
+    {
+      traditional: "我在師大下車，沒搭到台北火車站。",
+      simplified: "我在师大下车，没搭到台北火车站。",
+      pinyin: "wǒ zài shī dà xià chē méi dā dào tái běi huǒ chē zhàn",
+      english:
+        "I got off at the national taiwan normal university, but didn't get to taipei railway station.",
+    },
+    {
+      traditional: "張老師跟他太太決定不搬到西班牙了。",
+      simplified: "张老师跟他太太决定不搬到西班牙了。",
+      pinyin: "zhāng lǎo shī gēn tā tài tài jué dìng bù bān dào xī bān yá le",
+      english: "Teacher zhang and his wife decided not to move to spain.",
+    },
+    {
+      traditional: "我太累了，沒走到故宮，就回家了。",
+      simplified: "我太累了，没走到故宫，就回家了。",
+      pinyin: "wǒ tài lèi le méi zǒu dào gù gōng jiù huí jiā le",
+      english:
+        "I was so tired that i went home without going to the forbidden city.",
+    },
+    {
+      traditional: "我的事，你明天會拿到學校給我嗎？",
+      simplified: "我的事，你明天会拿到学校给我吗？",
+      pinyin: "wǒ dē shì nǐ míng tiān huì ná dào xué xiào gěi wǒ ma",
+      english: "My business, will you get it to school tomorrow?",
+    },
+    {
+      traditional: "那個電腦，你送到他家去了沒有？",
+      simplified: "那个电脑，你送到他家去了没有？",
+      pinyin: "nà gè diàn nǎo nǐ sòng dào tā jiā qù le méi yǒu",
+      english: "Did you send that computer to his house?",
+    },
+    {
+      traditional: "本子和筆，你是不是都放到背包裡了？",
+      simplified: "本子和笔，你是不是都放到背包里了？",
+      pinyin: "běn zi hé bǐ nǐ shì bú shì dōu fàng dào bēi bāo lǐ le",
+      english: "Did you put the notebook and pen in your backpack?",
+    },
+    {
+      traditional: "田中誠一付給房東三個月的房租。",
+      simplified: "田中诚一付给房东三个月的房租。",
+      pinyin: "tián zhōng chéng yī fù gěi fáng dōng sān gè yuè dē fáng zū",
+      english: "Seiichi tanaka paid the landlord three months' rent.",
+    },
+    {
+      traditional: "王先生賣給他一輛機車。",
+      simplified: "王先生卖给他一辆机车。",
+      pinyin: "wáng xiān shēng mài gěi tā yī liàng jī chē",
+      english: "Mr. wang sold him a locomotive.",
+    },
+    {
+      traditional: "他從沒國回來，送給白如玉一些沒國甜點。",
+      simplified: "他从没国回来，送给白如玉一些没国甜点。",
+      pinyin:
+        "tā cóng méi guó huí lái sòng gěi bái rú yù yī xiē méi guó tián diǎn",
+      english:
+        "He came back from the country and gave bai ruyu some desserts from the country.",
+    },
+    {
+      traditional: "他想買給我朋友這個電腦。",
+      simplified: "他想买给我朋友这个电脑。",
+      pinyin: "tā xiǎng mǎi gěi wǒ péng yǒu zhè gè diàn nǎo",
+      english: "He wants to buy this computer for my friend.",
+    },
+    {
+      traditional: "語言中心主任昨天才發給白如玉上個月的薪水。",
+      simplified: "语言中心主任昨天才发给白如玉上个月的薪水。",
+      pinyin:
+        "yǔ yán zhōng xīn zhǔ rèn zuó tiān cái fā gěi bái rú yù shàng gè yuè dē xīn shuǐ",
+      english:
+        "The director of the language center only gave bai ruyu last month's salary yesterday.",
+    },
+    {
+      traditional: "我打算送給我同學這些桌子，椅子。",
+      simplified: "我打算送给我同学这些桌子，椅子。",
+      pinyin: "wǒ dǎ suàn sòng gěi wǒ tóng xué zhè xiē zhuō zi yǐ zi",
+      english: "I plan to give these tables and chairs to my classmates.",
+    },
+    {
+      traditional: "他想買這個電腦給我朋友。",
+      simplified: "他想买这个电脑给我朋友。",
+      pinyin: "tā xiǎng mǎi zhè gè diàn nǎo gěi wǒ péng yǒu",
+      english: "He wants to buy this computer for my friend.",
+    },
+    {
+      traditional: "語言中心主任昨天才發上個月的薪水給白如玉。",
+      simplified: "语言中心主任昨天才发上个月的薪水给白如玉。",
+      pinyin:
+        "yǔ yán zhōng xīn zhǔ rèn zuó tiān cái fā shàng gè yuè dē xīn shuǐ gěi bái rú yù",
+      english:
+        "The director of the language center only issued last month's salary to bai ruyu yesterday.",
+    },
+    {
+      traditional: "我打算送這些桌子，椅子給我同學。",
+      simplified: "我打算送这些桌子，椅子给我同学。",
+      pinyin: "wǒ dǎ suàn sòng zhè xiē zhuō zi yǐ zi gěi wǒ tóng xué",
+      english: "I plan to give these tables and chairs to my classmates.",
+    },
+    {
+      traditional: "這個電腦，他想買給我朋友。",
+      simplified: "这个电脑，他想买给我朋友。",
+      pinyin: "zhè gè diàn nǎo tā xiǎng mǎi gěi wǒ péng yǒu",
+      english: "He wants to buy this computer for my friend.",
+    },
+    {
+      traditional: "上個月的薪水，語言中心主任昨天才發給巴如玉。",
+      simplified: "上个月的薪水，语言中心主任昨天才发给巴如玉。",
+      pinyin:
+        "shàng gè yuè dē xīn shuǐ yǔ yán zhōng xīn zhǔ rèn zuó tiān cái fā gěi bā rú yù",
+      english:
+        "Last month’s salary was only given to ba ruyu yesterday by the director of the language center.",
+    },
+    {
+      traditional: "這些桌子，椅子，我打算送給我同學。",
+      simplified: "这些桌子，椅子，我打算送给我同学。",
+      pinyin: "zhè xiē zhuō zi yǐ zi wǒ dǎ suàn sòng gěi wǒ tóng xué",
+      english: "I plan to give these tables and chairs to my classmates.",
+    },
+    {
+      traditional: "白如玉只要租三個月，所以那個房間，房東不租給他了。",
+      simplified: "白如玉只要租三个月，所以那个房间，房东不租给他了。",
+      pinyin:
+        "bái rú yù zhǐ yào zū sān gè yuè suǒ yǐ nà gè fáng jiān fáng dōng bù zū gěi tā le",
+      english:
+        "Bai ruyu only needs to rent for three months, so the landlord will not rent him that room.",
+    },
+    {
+      traditional: "田中誠一還沒付給房東上個月的房租。",
+      simplified: "田中诚一还没付给房东上个月的房租。",
+      pinyin:
+        "tián zhōng chéng yī hái méi fù gěi fáng dōng shàng gè yuè dē fáng zū",
+      english: "Seiichi tanaka hasn't paid the landlord last month's rent.",
+    },
+    {
+      traditional: "因為陳小姐快回國了，所以我不給他介紹工作了。",
+      simplified: "因为陈小姐快回国了，所以我不给他介绍工作了。",
+      pinyin:
+        "yīn wèi chén xiǎo jiě kuài huí guó le suǒ yǐ wǒ bù gěi tā jiè shào gōng zuò le",
+      english:
+        "Because miss chen is about to return to china, i will not introduce him to work.",
+    },
+    {
+      traditional: "你的腳踏車，賣給白如玉了沒有？",
+      simplified: "你的脚踏车，卖给白如玉了没有？",
+      pinyin: "nǐ dē jiǎo tà chē mài gěi bái rú yù le méi yǒu",
+      english: "Have you sold your bicycle to bai ruyu?",
+    },
+    {
+      traditional: "那些芒果跟甜點，你是不是送給陳月美了？",
+      simplified: "那些芒果跟甜点，你是不是送给陈月美了？",
+      pinyin:
+        "nèi xiē máng guǒ gēn tián diǎn nǐ shì bú shì sòng gěi chén yuè měi le",
+      english: "Did you give those mangoes and desserts to chen yuemei?",
+    },
+    {
+      traditional: "下個月中文課的學費，你付給學校了沒有？",
+      simplified: "下个月中文课的学费，你付给学校了没有？",
+      pinyin:
+        "xià gè yuè zhōng wén kè dē xué fèi nǐ fù gěi xué xiào le méi yǒu",
+      english:
+        "Have you paid the school fees for the chinese class next month?",
+    },
+    {
+      traditional: "我要賣我朋友這個電腦。",
+      simplified: "我要卖我朋友这个电脑。",
+      pinyin: "wǒ yào mài wǒ péng yǒu zhè gè diàn nǎo",
+      english: "I want to sell my friend's computer.",
+    },
+    {
+      traditional: "我打算送我同學這些桌子，椅子。",
+      simplified: "我打算送我同学这些桌子，椅子。",
+      pinyin: "wǒ dǎ suàn sòng wǒ tóng xué zhè xiē zhuō zi yǐ zi",
+      english: "I plan to send these tables and chairs to my classmates.",
+    },
+    {
+      traditional: "你和道公司付他多少薪水嗎？",
+      simplified: "你和道公司付他多少薪水吗？",
+      pinyin: "nǐ hé dào gōng sī fù tā duō shǎo xīn shuǐ ma",
+      english: "How much do you and dao company pay him?",
+    },
+    {
+      traditional: "那張椅子，請你放在樓下。",
+      simplified: "那张椅子，请你放在楼下。",
+      pinyin: "nà zhāng yǐ zi qǐng nǐ fàng zài lóu xià",
+      english: "Please put that chair downstairs.",
+    },
+    {
+      traditional: "這些複雜的漢字，我要寫在本子上。",
+      simplified: "这些复杂的汉字，我要写在本子上。",
+      pinyin: "zhè xiē fù zá dē hàn zì wǒ yào xiě zài běn zi shàng",
+      english:
+        "I want to write these complicated chinese characters in a notebook.",
+    },
+    {
+      traditional: "履歷表先留在我這裡，有適合的工作再告訴你。",
+      simplified: "履历表先留在我这里，有适合的工作再告诉你。",
+      pinyin:
+        "lǚ lì biǎo xiān liú zài wǒ zhè lǐ yǒu shì hé dē gōng zuò zài gào sù nǐ",
+      english:
+        "Leave the resume with me first, and let me tell you if there is a suitable job.",
+    },
+    {
+      traditional: "書別放在椅子上，請拿到房間去。",
+      simplified: "书别放在椅子上，请拿到房间去。",
+      pinyin: "shū bié fàng zài yǐ zi shàng qǐng ná dào fáng jiān qù",
+      english: "Don't put the book on the chair, please take it to the room.",
+    },
+    {
+      traditional: "我妹留甜點在桌子上，我把甜點吃了。",
+      simplified: "我妹留甜点在桌子上，我把甜点吃了。",
+      pinyin: "wǒ mèi liú tián diǎn zài zhuō zi shàng wǒ bǎ tián diǎn chī le",
+      english: "My sister left dessert on the table, and i ate the dessert.",
+    },
+    {
+      traditional: "他的錢沒存在銀行裡，他太太很不高興。",
+      simplified: "他的钱没存在银行里，他太太很不高兴。",
+      pinyin: "tā dē qián méi cún zài yín xíng lǐ tā tài tài hěn bù gāo xìng",
+      english: "His money is not in the bank, and his wife is very upset.",
+    },
+    {
+      traditional: "電視，你打算放在哪裡？",
+      simplified: "电视，你打算放在哪里？",
+      pinyin: "diàn shì nǐ dǎ suàn fàng zài nǎ lǐ",
+      english: "Tv, where are you going to put it?",
+    },
+    {
+      traditional: "這些家具，你是不是要留在這裡，不搬到新家？",
+      simplified: "这些家具，你是不是要留在这里，不搬到新家？",
+      pinyin:
+        "zhè xiē jiā jù nǐ shì bú shì yào liú zài zhè lǐ bù bān dào xīn jiā",
+      english:
+        "These furniture, do you want to stay here and not move to a new home?",
+    },
+    {
+      traditional: "我給你的資料，你存在電腦裡沒有？",
+      simplified: "我给你的资料，你存在电脑里没有？",
+      pinyin: "wǒ gěi nǐ dē zī liào nǐ cún zài diàn nǎo lǐ méi yǒu",
+      english: "Do you keep the information i gave you in your computer?",
+    },
+    {
+      traditional: "我把球踢到學校外面了。",
+      simplified: "我把球踢到学校外面了。",
+      pinyin: "wǒ bǎ qiú tī dào xué xiào wài miàn le",
+      english: "I kicked the ball outside the school.",
+    },
+    {
+      traditional: "他想把公司搬到台南。",
+      simplified: "他想把公司搬到台南。",
+      pinyin: "tā xiǎng bǎ gōng sī bān dào tái nán",
+      english: "He wants to move the company to tainan.",
+    },
+    {
+      traditional: "我打算把這個蛋糕那到學校請同學吃。",
+      simplified: "我打算把这个蛋糕那到学校请同学吃。",
+      pinyin: "wǒ dǎ suàn bǎ zhè gè dàn gāo nà dào xué xiào qǐng tóng xué chī",
+      english:
+        "I plan to take this cake to the school and invite my classmates to eat.",
+    },
+    {
+      traditional: "我要把這吃茶送給老師。",
+      simplified: "我要把这吃茶送给老师。",
+      pinyin: "wǒ yào bǎ zhè chī chá sòng gěi lǎo shī",
+      english: "I want to give this tea to the teacher.",
+    },
+    {
+      traditional: "請你把那衣服服拿給白如玉。",
+      simplified: "请你把那衣服服拿给白如玉。",
+      pinyin: "qǐng nǐ bǎ nà yī fú fu ná gěi bái rú yù",
+      english: "Please take that clothes to bai ruyu.",
+    },
+    {
+      traditional: "我打算把舊車賣給高先生",
+      simplified: "我打算把旧车卖给高先生",
+      pinyin: "wǒ dǎ suàn bǎ jiù chē mài gěi gāo xiān shēng",
+      english: "I plan to sell my old car to mr. gao",
+    },
+    {
+      traditional: "請你把那張椅子放在樓下。",
+      simplified: "请你把那张椅子放在楼下。",
+      pinyin: "qǐng nǐ bǎ nà zhāng yǐ zi fàng zài lóu xià",
+      english: "Please put that chair downstairs.",
+    },
+    {
+      traditional: "我要把媽媽給我的錢存在銀行裡。",
+      simplified: "我要把妈妈给我的钱存在银行里。",
+      pinyin: "wǒ yào bǎ mā mā gěi wǒ dē qián cún zài yín xíng lǐ",
+      english: "I want to deposit the money my mother gave me in the bank.",
+    },
+    {
+      traditional: "他沒把資料帶到學校來，可是還好我帶了。",
+      simplified: "他没把资料带到学校来，可是还好我带了。",
+      pinyin: "tā méi bǎ zī liào dài dào xué xiào lái kě shì hái hǎo wǒ dài le",
+      english:
+        "He didn't bring the materials to the school, but fortunately i brought them.",
+    },
+    {
+      traditional: "別把那本事賣給別人，買給我吧。",
+      simplified: "别把那本事卖给别人，买给我吧。",
+      pinyin: "bié bǎ nà běn shì mài gěi bié rén mǎi gěi wǒ ba",
+      english: "Don't sell that ability to others, buy it to me.",
+    },
+    {
+      traditional: "我還沒把這個月的房租那給房東。",
+      simplified: "我还没把这个月的房租那给房东。",
+      pinyin: "wǒ hái méi bǎ zhè gè yuè dē fáng zū nà gěi fáng dōng",
+      english: "I haven't given this month's rent to the landlord.",
+    },
+    {
+      traditional: "他還沒把這個月的房租那給房東。",
+      simplified: "他还没把这个月的房租那给房东。",
+      pinyin: "tā hái méi bǎ zhè gè yuè dē fáng zū nà gěi fáng dōng",
+      english: "He has not given the rent for this month to the landlord.",
+    },
+    {
+      traditional: "他還沒把買電視的錢付給老闆。",
+      simplified: "他还没把买电视的钱付给老板。",
+      pinyin: "tā hái méi bǎ mǎi diàn shì dē qián fù gěi lǎo bǎn",
+      english: "He hasn't paid the boss for the tv.",
+    },
+    {
+      traditional: "別把隨身碟放在桌子上。",
+      simplified: "别把随身碟放在桌子上。",
+      pinyin: "bié bǎ suí shēn dié fàng zài zhuō zi shàng",
+      english: "Don't put the flash drive on the table.",
+    },
+    {
+      traditional: "他沒把錢放在我這裡，她帶回家了。",
+      simplified: "他没把钱放在我这里，她带回家了。",
+      pinyin: "tā méi bǎ qián fàng zài wǒ zhè lǐ tā dài huí jiā le",
+      english: "He didn't put the money with me, she took it home.",
+    },
+    {
+      traditional: "你是不是把我的書帶到學校來了？",
+      simplified: "你是不是把我的书带到学校来了？",
+      pinyin: "nǐ shì bú shì bǎ wǒ dē shū dài dào xué xiào lái le",
+      english: "Did you bring my book to school?",
+    },
+    {
+      traditional: "他把錢車騎到公司來了沒有？我等一下要用。",
+      simplified: "他把钱车骑到公司来了没有？我等一下要用。",
+      pinyin:
+        "tā bǎ qián chē qí dào gōng sī lái le méi yǒu wǒ děng yī xià yào yòng",
+      english:
+        "Did he ride the money cart to the company? i will use it later.",
+    },
+    {
+      traditional: "你把那本本子拿給老師了沒有？",
+      simplified: "你把那本本子拿给老师了没有？",
+      pinyin: "nǐ bǎ nà běn běn zi ná gěi lǎo shī le méi yǒu",
+      english: "Have you given that book to the teacher?",
+    },
+    {
+      traditional: "你是不是把照相機送給他了？",
+      simplified: "你是不是把照相机送给他了？",
+      pinyin: "nǐ shì bú shì bǎ zhào xiàng jī sòng gěi tā le",
+      english: "Did you give him the camera?",
+    },
+    {
+      traditional: "房東是不是把熱水器裝在於是外面了？",
+      simplified: "房东是不是把热水器装在于是外面了？",
+      pinyin:
+        "fáng dōng shì bú shì bǎ rè shuǐ qì zhuāng zài yú shì wài miàn le",
+      english: "Did the landlord install the water heater outside?",
+    },
+    {
+      traditional: "你把學費用在什麼他方了？",
+      simplified: "你把学费用在什么他方了？",
+      pinyin: "nǐ bǎ xué fèi yòng zài shén me tā fāng le",
+      english: "Where did you spend the tuition?",
+    },
+    {
+      traditional: "台灣的夏天熱而且濕。",
+      simplified: "台湾的夏天热而且湿。",
+      pinyin: "tái wān dē xià tiān rè ér qiě shī",
+      english: "Summer in taiwan is hot and humid.",
+    },
+    {
+      traditional: "如果多花一點時間我，一定能到便宜而且合適的。",
+      simplified: "如果多花一点时间我，一定能到便宜而且合适的。",
+      pinyin:
+        "rú guǒ duō huā yī diǎn shí jiān wǒ yí dìng néng dào pián yí ér qiě hé shì dē",
+      english:
+        "If i spend a little more time, i will definitely get a cheap and suitable one.",
+    },
+    {
+      traditional: "房東幫他裝了有線電視，而且還幫他裝了網路。",
+      simplified: "房东帮他装了有线电视，而且还帮他装了网路。",
+      pinyin:
+        "fáng dōng bāng tā zhuāng le yǒu xiàn diàn shì ér qiě hái bāng tā zhuāng le wǎng lù",
+      english:
+        "The landlord installed the cable tv for him, and also installed the internet for him.",
+    },
+    {
+      traditional: "做這個工作得會說中文，而且得有一些工作經驗。",
+      simplified: "做这个工作得会说中文，而且得有一些工作经验。",
+      pinyin:
+        "zuò zhè gè gōng zuò dé huì shuō zhōng wén ér qiě dé yǒu yī xiē gōng zuò jīng yàn",
+      english:
+        "You must speak chinese and have some work experience to do this job.",
+    },
+  ],
 };
 
 export default lesson;
