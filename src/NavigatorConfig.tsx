8ee11be9819b747754d8abe4b8b612bd24608a90
Sean Smith <sean.smith.2009@gmail.com>
2021-08-16 17:40:34 -0700

    Try detachPreviousScreen

--- src/NavigatorConfig.tsx
+++ src/NavigatorConfig.tsx
@@ -347,8 +347,7 @@ const createAppNavigationStack = (firstTimeUser: boolean) => {
     initialRouteName: firstTimeUser ? ROUTE_NAMES.WELCOME : ROUTE_NAMES.HOME,
     defaultNavigationOptions: {
       cardStyle: { backgroundColor: "black" },
-      // detachPreviousScreen: true,
-      cardShadowEnabled: false,
+      detachPreviousScreen: true,
     },
   });
 };
